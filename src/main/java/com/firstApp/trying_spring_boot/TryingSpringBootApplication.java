08c4abc4c9b27f75c0945730291cd0075326a74a
kartik <j.kartik0209@gmail.com>
2024-12-04 17:30:22 +0530

    first spring project

---
+++ src/main/java/com/firstApp/trying_spring_boot/TryingSpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.firstApp.trying_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryingSpringBootApplication.class, args);
+		System.out.println("Welcome");
+	}
+
+
+}
---
+++ src/test/java/com/firstApp/trying_spring_boot/TryingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstApp.trying_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryingSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
